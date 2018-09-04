c504c526845337556a07e35c1041dcafe368e28c
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-09-04 00:35:25 -0400

    v1.0

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// array of word options for computer to use
+var targetWords = ["Gilligan", "Skipper", "Professor", "Ginger", "Mary Ann", "Thurston Howell III", "Lovey Howell"];
+var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// break down the chosen word into its component letters and display empty spaces 
+// capture user letter guesses
